afc07a94918397f85560237bb812020354754966
Ben Russell <thematrixeatsyou@gmail.com>
2018-09-20 20:05:55 +0000

    Remove debug line

--- tools/build.sh
+++ tools/build.sh
@@ -13,7 +13,6 @@ fi
 echo "dragonpup:"
 cc -DSUPER_ZZT=0 -O1 -g -o bin/dragonpup src/*.c -I src/ && \
 ./bin/dragonpup --unit-tests | tap_harness
-echo $?
 
 echo "super-dragonpup:"
 cc -DSUPER_ZZT=1 -O1 -g -o bin/super-dragonpup src/*.c -I src/ && \
