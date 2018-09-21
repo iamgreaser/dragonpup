c8d0866c65a738898b3ec03ab641377ee680db3e
Ben Russell <thematrixeatsyou@gmail.com>
2018-09-21 05:55:32 +0000

    Fix build+test script goofups

--- tools/build.sh
+++ tools/build.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 
 # Pick a suitable TAP harness
-if xpython3 -c "import tap.main" >/dev/null 2>&1; then
+if python3 -c "import tap.main" >/dev/null 2>&1; then
 	tap_harness () { python3 -c 'import tap.main; tap.main.main(["-c", "-"])'; }
-elif xpython -c "import tap.main" >/dev/null 2>&1; then
+elif python -c "import tap.main" >/dev/null 2>&1; then
 	tap_harness () { python -c 'import tap.main; tap.main.main(["-c", "-"])'; }
 else
 	# Fallback: Print failing tests
-	tap_harness () { if grep '^not ok'; then false; else true; fi; }
+	tap_harness () { if grep '^not ok'; then false; else echo "OK!"; true; fi; }
 fi
 
 echo "dragonpup:"
 cc -DSUPER_ZZT=0 -O1 -g -o bin/dragonpup src/*.c -I src/ && \
-./bin/dragonpup --unit-tests | tap_harness && \
-echo "OK!"
+./bin/dragonpup --unit-tests | tap_harness
 
 echo "super-dragonpup:"
 cc -DSUPER_ZZT=1 -O1 -g -o bin/super-dragonpup src/*.c -I src/ && \
-./bin/super-dragonpup --unit-tests | tap_harness && \
-echo "OK!"
+./bin/super-dragonpup --unit-tests | tap_harness
 
