b9a62e10cdbc9e6cd365f30f960beb31a77bc2f4
Ben Russell <thematrixeatsyou@gmail.com>
2018-09-23 19:33:56 +0000

    Initial block creation + freeing

--- src/board.c
+++ src/board.c
@@ -1,6 +1,56 @@
 #include "common.h"
 #include "board.h"
 
+////////////////////////////////////////////////////////////////////////////
+
+Block *new_block(int width, int height)
+{
+	Block *block = malloc(sizeof(Block));
+
+	memset(block, 0, sizeof(block));
+
+	return block;
+}
+
+void free_block(Block **pblock)
+{
+	if(*pblock != NULL)
+	{
+		if((*pblock)->tile_data != NULL)
+		{
+			free((*pblock)->tile_data);
+			(*pblock)->tile_data = NULL;
+		}
+
+		free(*pblock);
+	}
+
+	*pblock = NULL;
+}
+
+static void TEST_new_block(void)
+{
+	Block *block;
+
+	// ZZT-sized board block
+	block = new_block(60, 25);
+	tap_ok(block != NULL, "Create block: (60, 25)");
+	free_block(&block);
+	tap_ok(true, "Block can be freed");
+	tap_ok(block == NULL, "Block set to NULL when freed");
+	free_block(&block);
+	tap_ok(true, "Freeing NULL blocks causes no pain");
+	tap_ok(block == NULL, "NULL block stays NULL");
+
+	// Super ZZT-sized board block
+	block = new_block(96, 80);
+	tap_ok(block != NULL, "Create block: (96, 80)");
+	free_block(&block);
+
+}
+
+////////////////////////////////////////////////////////////////////////////
+
 void board_tests(void)
 {
 	tap_ok(true, "Board test hookup check");
--- src/board.h
+++ src/board.h
@@ -1,1 +1,10 @@
+//# new_block
+Block *new_block(int width, int height);
+
+//# free_block: Frees the given block if not NULL.
+//#
+//# block:
+//#   Double-pointer to block. Pointer to block is set to NULL.
+void free_block(Block **pblock);
+
 
