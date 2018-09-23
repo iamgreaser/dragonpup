8c80a20263172626192ce760c2f28e7306495ed3
Ben Russell <thematrixeatsyou@gmail.com>
2018-09-23 20:03:03 +0000

    Add tile colour/char gets to header

--- src/tile.h
+++ src/tile.h
@@ -4,3 +4,10 @@ tile_type get_tile_type_from_name(const char *name);
 //# get_tile_name_from_type
 const char *get_tile_name_from_type(tile_type type);
 
+//# get_tile_char
+char get_tile_char(const Tile *tile, const Stat *stat,
+	const Block *block, int x, int y);
+
+//# get_tile_color
+uint8_t get_tile_color(const Tile *tile, const Stat *stat,
+	const Block *block, int x, int y);
